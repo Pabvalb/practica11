9d5c36ff9a8d305b3f2498ce4fc858dfa835241b
Pabvalb <pabvalb02@gmail.com>
2026-04-30 13:37:33 -0600

    Create programa1.c

---
+++ programa1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+void imprimir(char[]);
+int main()
+{
+    char nombre[] = "Facultad de Ingeniería"; 
+    imprimir(nombre);
+}
+    void imprimir(char s[])
+    { 
+      int tam;
+      for ( tam=strlen(s)-1 ; tam>=0 ; tam--) 
+          printf("%c", s[tam]);
+      printf("\n"); 
+    }
